86ee1ba7c728ae6fd98735df0f018c96ce16cc6b
bilguune04 <86903088+bilguune04@users.noreply.github.com>
2026-04-08 13:54:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function search() {
+  alert("Search button clicked!");
+}
+
+function goExplore() {
+  window.location.href = "explore.html";
+}
+
+function goDetail() {
+  window.location.href = "detail.html";
+}
